96f702d5b5bf526170a5b2807cafcb16f29913bc
mixpap <mighalis@gmail.com>
2022-03-24 11:10:35 +0200

    update aliases setup

--- setup_alias.sh
+++ setup_alias.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i "/alias lab*/d" ~/.bashrc
-echo "alias lab='conda activate lab; jupyter notebook ~/lab_kormou/'" >> ~/.bashrc
+echo "alias lab_update ='cd ~/lab_kormou/; git reset --hard HEAD; git pull'" >> ~/.bashrc
+echo "alias lab='conda activate lab; mkdir ~/lab_tmp; cp ~/lab_kormou/* ~/lab_tmp/.; jupyter notebook ~/lab_tmp/'" >> ~/.bashrc
 bash
